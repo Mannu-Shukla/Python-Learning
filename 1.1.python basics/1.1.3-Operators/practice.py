b485285b930afbdf04542d61e09719faba66e23a
mannu Shukla <mannushukla810@gmail.com>
2026-07-17 23:33:23 +0530

    operator completed

---
+++ 1.1.python basics/1.1.3-Operators/practice.py
@@ -0,0 +1,32 @@
+# Question 1
+a = 13
+b = 14
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
+
+# Question 2
+age = 20
+
+print(age >= 18)
+print(age < 18)
+
+# Question 3
+marks = 75
+
+print(marks > 50 and marks < 100)
+print(marks > 90 or marks > 50)
+
+# Question 4
+is_student = True
+
+print(not is_student)
+
+# Question 5
+print(5 + 2 * 4)
+print((5 + 2) * 4)
